ce3506c484f8d60043e88312651fccaa3263955e
andrius-siup <andrius.siupinys@gmail.com>
2021-02-27 14:34:45 +0000

    Define variable to find how many .lead class are in the html document.

--- js/transcript.js
+++ js/transcript.js
@@ -4,4 +4,5 @@ var article = document.getElementById('home-page');
 // Returns a list of elements
 var sections = document.getElementsByTagName('section');
 
+var leads = document.getElementsByClassName('lead');
 
