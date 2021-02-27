ecf55ae6993a569eae7b74ded81d6b3b912b6325
andrius-siup <andrius.siupinys@gmail.com>
2021-02-27 14:33:01 +0000

    Define variable sections to figure out how many section element are.

--- js/transcript.js
+++ js/transcript.js
@@ -1,4 +1,7 @@
-// returns single element.
+// returns single element
 var article = document.getElementById('home-page');
+
+// Returns a list of elements
+var sections = document.getElementsByTagName('section');
 
 
